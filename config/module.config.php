12a90f88cc72bc2124927f4ae12aba21565e0f6f
4orever <riv82girsul>
2015-05-13 15:45:11 +0300

    WTF?!

--- config/module.config.php
+++ config/module.config.php
@@ -6,5 +6,17 @@ return [
         'factories' => [
             'log' => 'PpcDevMode\Factory\Log',
         ]
-    ]
+    ],
+    'asset_manager' => array(
+        'resolver_configs' => array(
+            'paths' => array(
+                'ppc-backend-client' => __DIR__ . '/../src/',
+            ),
+            'map' => [
+                'framework.js' => __DIR__ . '/../src/build/production/App/framework.js',
+                'bootstrap.js' => __DIR__ . '/../src/bootstrap.js',
+                'bootstrap.json' => __DIR__ . '/../src/bootstrap.json'
+            ]
+        ),
+    )
 ];
